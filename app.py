34d43d3200a499c75d9a694121e0a2063cb9688f
Dannydoesdev <daniel.t.mcgee@gmail.com>
2022-04-28 23:38:15 +1000

    testing heroku

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 import psycopg2
-
-from flask import Flask
 import os
+import bcrypt
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'dbname=project2')
+from flask import Flask, request, redirect, render_template, session
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'dbname=flyre')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'pretend secret key for testing')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+
+
 @app.route('/')
 def index():
     conn = psycopg2.connect(DATABASE_URL)
-    cur =  conn.cursor()
-    cur.execute('SELECT 1', [])
-    conn.close()
+    cur = conn.cursor()
+    cur.execute('SELECT * from USERS')
+    results = cur.fetchall()
+    print(results)
+    # email = session.get('email')
+    # name = session.get('name')
+    # id = session.get('id')
+    # if email:
+    #     print(email)
+    # if name:
+    #     print(name)
+    # if id:
+    #     print(id)
+
+    return render_template('index.html', results=results)
+
+    # conn = psycopg2.connect(DATABASE_URL)
+    # cur =  conn.cursor()
+    # cur.execute('SELECT 1', [])
+    # conn.close()
     
-    return 'Hello World!'
+    # return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
